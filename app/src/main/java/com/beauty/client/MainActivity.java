55647a6ec9fc7fd4680e505b5a1a1eeae6fb4b81
zhuqi259 <zhu.qi.memory@gmail.com>
2016-07-27 20:04:05 +0800

    测试显示图片

--- app/src/main/java/com/beauty/client/MainActivity.java
+++ app/src/main/java/com/beauty/client/MainActivity.java
@@ -2,12 +2,25 @@ package com.beauty.client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import com.beauty.client.utils.VolleyLoadPicture;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    public static final String picUrl = "http://img.quwenjiemi.com/2014/0701/thumb_420_234_20140701112917406.jpg";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //用Volley加载图片
+        VolleyLoadPicture vlp = new VolleyLoadPicture(this, imageView);
+        vlp.getmImageLoader().get(picUrl, vlp.getOne_listener());
     }
 }
